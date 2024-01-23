0c962c289fce980e7d75bf8e7dd078ae2dad7f4d
feramin108 <43932539+feramin108@users.noreply.github.com>
2024-01-23 19:24:12 +0300

    first  commint

--- Containerization.sh
+++ Containerization.sh
@@ -14,23 +14,4 @@ docker-compose up -d
 # Task 4
 # Configure Nginx Load Balancer (config file already created)
 
-# Initialize Git repository
-git init
-
-# Set Git user identity
-git config --global user.email "feramin108@gmail.com"
-git config --global user.name "feramin108"
-
-# Check if a remote named 'origin' exists
-if git remote | grep -q 'origin'; then
-  # Remote 'origin' already exists, update the branch name
-  git branch -M main
-else
-  # Remote 'origin' does not exist, add the remote
-  git remote add origin https://github.com/feramin108/containers.git
-fi
-
-# Commit and push to GitHub
-git add .
-git commit -m "Initial commit"
-git push -u origin main
+#
